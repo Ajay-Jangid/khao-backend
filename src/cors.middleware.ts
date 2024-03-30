f7ee92ec15a181157e2b39d0e962445de0217a12
Ajay-Jangid <ajay11071999@gmail.com>
2024-03-30 19:12:15 +0530

    changes

---
+++ src/cors.middleware.ts
@@ -0,0 +1,15 @@
+// cors.middleware.ts
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        // Add CORS headers for outgoing responses
+        res.header('Access-Control-Allow-Origin', 'https://khao-backend.vercel.app');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        next();
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsMiddleware } from './cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.enableCors()
+  app.use(CorsMiddleware);
   await app.listen(3000);
 }
 bootstrap();
